42af71ccf435d34d172f2b71362d3dc85b032f7a
Bendik Nordeng <bendiknor@gmail.com>
2020-02-19 22:05:59 +0100

    Comment criticNN

--- Project1/criticNN.py
+++ Project1/criticNN.py
@@ -10,31 +10,35 @@ class CriticNN:
         self.lam = lam
         self.gamma = gamma
         self.eligibilities = []
-        # Build model
+        # Build model of type Sequential()
         self.model = Sequential()
         self.model.add(Dense(inputLayerSize, activation='relu', input_dim=inputLayerSize))
         for i in range(len(hiddenLayerSizes)):
             self.model.add(Dense(hiddenLayerSizes[i], activation='relu'))
-        self.model.add(Dense(1, activation='linear'))
+        self.model.add(Dense(1))
 
         self.resetEligibilities()
         adagrad = tf.keras.optimizers.Adagrad(learning_rate=self.alpha)
         self.model.compile(optimizer=adagrad,loss=tf.keras.losses.MeanSquaredError(), run_eagerly = True)
 
+    # generate eligibilities with equal shape as trainable weights
     def resetEligibilities(self):
         self.eligibilities.clear()
-        for params in self.model.trainable_weights:
+        for params in self.model.trainable_variables:
             self.eligibilities.append(tf.zeros_like(params))
 
+    # decay eligibilities with factor gamma*lambda
     def updateEligibilities(self):
         for i in range(len(self.eligibilities)):
             self.eligibilities[i] = self.lam * self.gamma * self.eligibilities[i]
 
+    # return the model's current valuation of given state
     def stateValue(self, state):
-        state = [tf.strings.to_number(bin, out_type=tf.dtypes.int32) for bin in state]
+        state = [tf.strings.to_number(bin, out_type=tf.dtypes.int32) for bin in state] # convert to array
         state = tf.convert_to_tensor(np.expand_dims(state, axis=0))
         return self.model(state).numpy()[0][0]
 
+    # given the model's valuation of lastState and state, return td_error
     def findTDError(self, reinforcement, lastState, state):
         target = reinforcement + self.gamma * self.stateValue(state)
         td_error = target - self.stateValue(lastState)
@@ -42,11 +46,12 @@ class CriticNN:
 
     def modify_gradients(self, gradients, td_error):
         for j in range(len(gradients)):
-            gradients[j] = gradients[j] * 1/(2*td_error)
-            self.eligibilities[j] = tf.add(self.eligibilities[j], gradients[j])
-            gradients[j] = self.eligibilities[j] * td_error
+            gradients[j] = gradients[j] * 1/(2*td_error) # retrieve gradient value of state w.r.t. weights
+            self.eligibilities[j] = tf.add(self.eligibilities[j], gradients[j]) # adjust eligibilities with current gradient
+            gradients[j] = self.eligibilities[j] * td_error # gradients to be applied by optimizer
         return gradients
 
+    # caluclate loss and apply modified gradients to weights via model optimizer
     def fit(self, reinforcement, lastState, state, td_error):
         with tf.GradientTape() as tape:
             lastState, state, gamma, reinforcement = self.convertData(lastState, state, self.gamma, reinforcement)
@@ -57,6 +62,7 @@ class CriticNN:
         modified_gradients = self.modify_gradients(gradients, td_error)
         self.model.optimizer.apply_gradients(zip(modified_gradients, self.model.trainable_variables))
 
+    # convert input to tensors
     def convertData(self, lastState, state, gamma, reinforcement):
         lastState = [tf.strings.to_number(bin, out_type=tf.dtypes.float32) for bin in lastState]  # convert to array
         lastState = tf.convert_to_tensor(np.expand_dims(lastState, axis=0))
