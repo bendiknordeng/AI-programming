245b809af032d3df5051c54d2b14a99837baf930
Bendik Nordeng <bendiknor@gmail.com>
2020-02-19 21:36:45 +0100

    Comment actor

--- Project1/actor.py
+++ Project1/actor.py
@@ -1,10 +1,6 @@
 import random
 import numpy as np
 
-# policy updates occur within the actor
-# The actor should map from state, action to z (a real number)
-# The actor must keep track of the results of performing actions in states.
-
 class Actor:
     def __init__(self, alpha, lam, gamma):
         self.alpha = alpha
@@ -13,35 +9,40 @@ class Actor:
         self.saps = {}
         self.eligs = {}
 
+    # create state action pair for given state and actions
     def createSAPs(self, state, actions):
         for fromP in actions:
             for toP in actions[fromP]:
-                if self.saps.get((state, (fromP, toP))) == None:
+                if self.saps.get((state, (fromP, toP))) == None: # if given SAP is not yet created
                     self.saps[(state, (fromP, toP))] = 0
 
-    def findNextAction(self, currentState, validActions, eps):
+    # assign value of state action pairs, and choose greedy with prob 1-eps, else choose random
+    def findNextAction(self, state, actions, eps):
         actionStack = {}
-        for fromP in validActions:
-            for toP in validActions[fromP]:
-                actionStack[(fromP, toP)] = self.saps[(currentState, (fromP, toP))]
-
+        for fromP in actions:
+            for toP in actions[fromP]:
+                actionStack[(fromP, toP)] = self.saps[(state, (fromP, toP))]
         if len(actionStack) > 0:
             if random.random() < eps:
                 return random.choice(list(actionStack.items()))[0]
             else:
                 return max(actionStack, key=actionStack.get)
 
+    # based on td_error, update state action pairs with learning rate alpha
     def updateSAPs(self, td_error):
         for sap in self.eligs:
-            if sap[1] != None:
+            if sap[1] != None: # last state has no valid actions
                 self.saps[sap] = self.saps[sap] + self.alpha * td_error * self.eligs[sap]
 
+    # assign 1 to last chosen state action pair
     def updateEligibility(self, state, action):
         self.eligs[(state, action)] = 1
 
+    # decay eligibilities with factor gamma*lambda
     def decayEligibilities(self):
         for sap in self.eligs:
             self.eligs[sap] = self.gamma * self.lam * self.eligs[sap]
 
+    # remove all eligibilities to make new trace
     def resetEligibilities(self):
         self.eligs.clear()
