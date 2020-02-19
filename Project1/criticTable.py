5594f5cbbb9a5d0b014f983cc2512ec1e1e7a6aa
Bendik Nordeng <bendiknor@gmail.com>
2020-02-19 22:08:21 +0100

    Comment criticTable

--- Project1/criticTable.py
+++ Project1/criticTable.py
@@ -8,6 +8,7 @@ class CriticTable:
         self.values = {}
         self.eligs = {}  # key, value is state, eligibility
 
+    # initialize state values with small, random numbers
     def createStateValues(self, state):
         if self.values.get(state) == None:
             self.values[state] = random.random()
@@ -16,20 +17,24 @@ class CriticTable:
         self.td_error = reinforcement + self.gamma * self.values[state] - self.values[lastState]
         return self.td_error
 
+    # update all state values in current trace
     def updateStateValues(self):
         for state in self.eligs:
             self.values[state] = self.values[state] + self.alpha * self.td_error * self.eligs[state]
 
+    # create eligibility wiht value 1 for last visited state
     def createEligibility(self, state):
         if self.eligs.get(state) == None:
             self.eligs[state] = 1
 
+    # decay eligibilities with factor gamma*lambda
     def updateEligibilities(self):
         for state in self.eligs:
             self.eligs[state] = self.gamma * self.lam * self.eligs[state]
 
     def resetEligibilities(self):
-        self.eligs.clear()
+        self.eligs.clear() # remove all eligibilities to make new trace
 
+    # return critic's valuation of input state
     def stateValue(self, state):
         return self.values[state]
