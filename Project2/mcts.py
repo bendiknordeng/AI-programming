833fc84dd148b8b32ac33df9bcaff52aeaa3cb93
Bendik Nordeng <bendiknor@gmail.com>
2020-02-26 23:49:27 +0100

    fix

--- Project2/mcts.py
+++ Project2/mcts.py
@@ -1,63 +1,38 @@
 from game import NIM, Ledge
 import random
 from tqdm import tqdm
-import matplotlib.pyplot as plt
-
 
 class MCTS:
-    def __init__(self, B, M, env, eps, eps_decay, c):
+    def __init__(self, B, M, c):
         self.G = G
         self.M = M
-        self.env = env
-        self.eps = eps
-        self.eps_decay = eps_decay
         self.c = c
 
-    def tree_search(self, display):
-        winner = []
-        iteration_number = []
-        for i in tqdm(range(self.G)):
+    def tree_search(self, game_mode, P, N, K, B, verbose):
+        win = 0
+        for i in range(self.G):
+            if game_mode == 0:
+                self.env = NIM(P, N, K)
+            else:
+                self.env = Ledge(P, B)
             current = self.env.root
+            if verbose: print("Initial state: {}".format(current.state))
+
             while not self.env.final_state(current.state):
-                current = self.__find_best_leaf(current)
                 for j in range(self.M):
                     self.__rollout(current)
-                if self.env.final_state(current.state):
-                    if current.parent.turn:
-                        winner.append(1)
-                    else:
-                        winner.append(2)
-                    break
-            iteration_number.append(i)
-            self.eps = self.eps * self.eps_decay
-        if display:
-            plt.plot(iteration_number, winner) # plot the development for each episode
-            plt.show()
+                current, _ = current.get_best_child()
+                if verbose: self.env.print_move(current)
 
-        print("Player one wins: {}\nPlayer two wins: {}".format(
-            winner.count(1), winner.count(2)))
+            if current.parent.turn == self.env.root.turn:
+                win += 1
+            if verbose:
+                print("Player {} wins after {} moves\n".format(
+                    1 if current.parent.turn else 2, current.count_parents()))
 
-    def run_greedy(self):
-        current = self.env.root
-        print("Initial state: {}".format(current.state))
-        while current.children:
-            current, _ = current.get_best_child()
-            self.env.print_move(current)
-        print('Player {} won after {} moves.\n'.format(
-            1 if current.parent.turn else 2, current.count_parents()))
+        print("Starting player wins: {}/{} ({}%)".format(
+            win, self.G, win * 100 / self.G))
 
-    def display_best_path(self):
-        current = self.env.root
-        print(current)
-        while current.children:
-            current, _ = current.get_best_child()
-            print(current)
-
-    def __find_best_leaf(self, node):
-        current = node
-        while current.children:
-            current, _ = current.get_best_child()
-        return current
 
     def __rollout(self, node):
         current = node
@@ -65,11 +40,8 @@ class MCTS:
             current.visits += 1
             if not current.children:
                 self.env.generate_child_states(current)
-            if random.random() > self.eps:
-                current, _ = current.get_best_child()
-            else:
-                current = random.choice(current.children)
-        current.visits += 1 # add visit to final node
+            current = random.choice(current.children)
+        current.visits += 1  # add visit to final node
         self.__backprop(current)
 
     def __backprop(self, node):
@@ -82,28 +54,17 @@ class MCTS:
                 break
 
 
-
 if __name__ == '__main__':
-    G = 100  # number of games in batch
+    G = 10  # number of games in batch
     M = 500  # number of rollouts per game move
-    P = 1  # (1/2/3): Player 1 starts/Player 2 starts/Random player starts
+    P = 1  # (1/2/3): Player 1 starts/Player 2 starts/Random player startsudfar
     c = 1  # exploration constant
-    eps = 1
-    eps_decay = 0.95
-    game_mode = 0  # (0/1): NIM/Ledge
-
     N = 10  # Inittial pile for NIM
     K = 3  # Max pieces for each action in NIM
-    B = [1, 1, 1, 0, 2, 0, 1]  # board for ledge
+    B = [1, 1, 0, 0, 2, 1]  # board for ledge
+    game_mode = 0  # (0/1): NIM/Ledge
 
-    if game_mode == 0:
-        env = NIM(P, N, K)
-    else:
-        env = Ledge(P, B)
+    mcts = MCTS(G, M, c)
 
-    display = True
-    mcts = MCTS(G, M, env, eps, eps_decay, c)
-    mcts.tree_search(display)
-    mcts.display_best_path()
-    print()
-    mcts.run_greedy()
+    verbose = True
+    mcts.tree_search(game_mode, P, N, K, B, verbose)
