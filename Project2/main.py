261c520714ca66d4f3e1706efb4849ac1b52dc1b
Bendik Nordeng <bendiknor@gmail.com>
2020-03-10 18:20:43 +0100

    Add function for last move

--- Project2/main.py
+++ Project2/main.py
@@ -10,6 +10,13 @@ def set_starting_player(P):
         return random.choice([1,2])
     return P
 
+def print_last_move(iteration, action, player, game_mode):
+    msg = ""
+    msg += "{}: ".format(iteration+1)
+    msg += NIMState.print_move(action.game_state, 3-player, 0) if game_mode == 0 else LedgeState.print_move(0, 3-player, action.game_state)
+    msg += "Player "+str(3-player)+" won\n\n"
+    return msg
+
 def run_batch(G, M, N, K, B, P, game_mode, verbose):
     win = 0
     verbose_message = ""
@@ -25,24 +32,21 @@ def run_batch(G, M, N, K, B, P, game_mode, verbose):
             if verbose:
                 verbose_message += "{}: ".format(iteration)
                 verbose_message += NIMState.print_move(action.prev_action, player, action.game_state) if game_mode == 0 else LedgeState.print_move(action.prev_action, player, action.parent.game_state)
-        verbose_message += "{}: ".format(iteration+1)
-        verbose_message += NIMState.print_move(action.game_state, 3-player, 0) if game_mode == 0 else LedgeState.print_move(0, 3-player, action.game_state)
-        verbose_message += "Player "+str(3-player)+" won\n\n"
         if initial_player == 3-player:
             win += 1
+        verbose_message += print_last_move(iteration, action, player, game_mode)
     if verbose: print(verbose_message)
     print("Starting player won {}/{} ({}%)".format(win, G, 100 * win / G))
 
 
 if __name__ == '__main__':
-    G = 5
+    G = 10
     M = 500
-    N = 15
-    K = 5
+    N = 10
+    K = 9
     B = [0, 0, 0, 2, 0, 1]
     P = 3
     game_mode = 0 # (0/1): NIM/Ledge
     verbose = True
 
     run_batch(G, M, N, K, B, P, game_mode, verbose)
-    #[print(get_best_action(M,1,B).game_state) for i in range(50)]
