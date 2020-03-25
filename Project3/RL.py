8f2f36abe801e04bed7fe806913fb96fb02f9428
Sander Grønli Nordeide <sandergn@stud.ntnu.no>
2020-03-25 22:38:42 +0100

    .

--- Project3/RL.py
+++ Project3/RL.py
@@ -11,6 +11,7 @@ np.set_printoptions(linewidth=160) # print formatting
 def RL_algorithm(games, simulations, env, ANN, eps_decay, epochs):
     cases = []
     MCTS = MonteCarloTreeSearch(ANN)
+    epoch_increment = math.ceil(100/float(games))
     for i in tqdm(range(games)):
         env.reset()
         MCTS.init_tree()
@@ -19,13 +20,12 @@ def RL_algorithm(games, simulations, env, ANN, eps_decay, epochs):
             action, D = MCTS.search(env, M)
             cases.append((env.flat_state,D))
             env.move(action)
-            #M = math.ceil(M*0.5)
+            #M = math.ceil(M*0.8)
         fit_cases = random.sample(cases, math.ceil(len(cases)/2))
         ANN.fit(fit_cases)
-        ANN.epochs += math.ceil(10/float(games)) #increment epochs
+        ANN.epochs += math.floor(np.exp(i/(games/3))) #increment epochs
         #if (i+1) % save_interval == 0:
         #    ANN.model.save_weights(model_path.format(level=i+1))
-
     #run through cases
     accuracies = []
     epochs = []
@@ -38,7 +38,7 @@ def RL_algorithm(games, simulations, env, ANN, eps_decay, epochs):
     accuracy = 0
     accuracies.append(ANN.accuracy(val_data))
     epochs.append(ANN.epochs*runs)
-    while accuracy < 0.8 and runs < 100: #run 10 * 30 epochs
+    while accuracy < 0.5 and runs < 100: #run 10 * 30 epochs
         runs += 1
         random.shuffle(train_data) #shuffle data before each split
         for i in range(4): # train ann on train_data
@@ -68,34 +68,25 @@ def play_game(dict, env, ANN, delay=-1,verbose=True):
             input = tuple(env.flat_state)
             print(input)
             if dict.get(input) != None:
-                for tar, pred in dict[input]:
-                    print(tar, pred)
+                targets = []
+                for tar, _ in dict[input]:
+                    targets.append(tar)
+                mean_target = np.around(np.mean(targets, axis = 0),decimals = 1)
+                print(mean_target)
             else:
                 print("No such case for input state")
-            print()
             print(np.around(probs.numpy()*100, decimals = 1))
             if delay > -1:
                 env.draw()
         else:
             if delay > -1:
                 env.draw(delay)
-
         preds.append(np.around(probs.numpy()*100, decimals = 1))
         moves.append(action)
         env.move(action)
         j += 1
     winning_player =  3 - env.flat_state[0]
     print("player", winning_player, "won after", j, "moves.")
-
-    """
-    print()
-    print()
-    print("played game")
-    for i in range(len(moves)):
-        print()
-        print(inputs[i])
-        print(preds[i], "  ",moves[i])
-    """
     if delay > -1:
         env.draw()
 
@@ -107,12 +98,10 @@ def say():
 
 if __name__ == '__main__':
     # Game parameters
-    board_size = 3
+    board_size = 4
 
     # MCTS/RL parameters
-
     episodes = 100
-
     simulations = 1000
 
     #training_batch_size = 100
@@ -122,22 +111,21 @@ if __name__ == '__main__':
     # ANN parameters
     activation_functions = ["linear", "sigmoid", "tanh", "relu"]
     optimizers = ["Adagrad", "SGD", "RMSprop", "Adam"]
-    alpha = 0.001 # learning rate
-    H_dims = [board_size, board_size**2]
+    alpha = 0.005 # learning rate
+    H_dims = [board_size*board_size]
     io_dim = board_size * board_size # input and output layer sizes (always equal)
     activation = activation_functions[3]
     optimizer = optimizers[3]
-
     epochs = 1
 
     for i in range(1):
         print(i)
         env = HexGame(board_size)
         ann = ANN(io_dim, H_dims, alpha, optimizer, activation, epochs)
-        prediction_dictionary = RL_algorithm(episodes, simulations, env, ann, eps_decay, epochs)
-        def play(dict = prediction_dictionary, env = env, ANN = ann):
+        pred_dict = RL_algorithm(episodes, simulations, env, ann, eps_decay, epochs)
+        def play(dict = pred_dict, env = env, ANN = ann):
             play_game(dict, env,ann,-1,0)
         play()
     #say()
 
-    #import pdb; pdb.set_trace()
+    import pdb; pdb.set_trace()
