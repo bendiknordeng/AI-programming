ae1d3790a03a0a40916baf4767e4ff55bbc8faa1
Sander Grønli Nordeide <sandergn@stud.ntnu.no>
2020-03-30 23:21:33 +0200

    Make TOPP.py

--- Project3/TOPP.py
+++ Project3/TOPP.py
@@ -1,35 +1,38 @@
-from game import HexState
-from RL import RL_algorithm
+from game import HexGame
+from RL import RL
 from ANN import ANN
 from collections import defaultdict
 import random
+import math
 
 class TOPP:
     def __init__(self, players, board_size):
         self.players = players
         self.board_size = board_size
+        self.env = HexGame(board_size)
 
-    def run_tournament(games, players):
-        results = defaultdict(int)
-        for player1 in players:
-            for player2 in players:
-                if player1 != player2:
-                    for _ in range(games):
-                        result = play_game(player1, player2)
-                        results[player1] += int(result)
-                        results[player2] += int(not result)
-            players.remove(player1)
-        print(results)
-
-    def play_game(player1, player2, display = False):
-        game = HexState(4)
-        turn = random.choice([True,False])
-        print("{} starts with red".format(player1 if turn else player2))
-        while not game.is_game_over():
-            game = player1.move(game) if turn else player2.move(game)
-            game.draw(1)
-            turn = not turn
-        return game, player2 if turn else player1
+    def run_tournament(self):
+        print(); print()
+        print("Tournament on a board size", self.board_size )
+        for p1 in self.players:
+            print()
+            for p2 in self.players:
+                if self.players[p1] != self.players[p2]:
+                    winner_stats = self.play_game(self.players[p1], self.players[p2])
+                    print("Player lvl", p1, "- player lvl", p2,": player", winner_stats )
+    def play_game(self, ann1, ann2, display = False):
+        self.env.reset()
+        j = 0
+        while not self.env.is_game_over():
+            j += 1
+            _, action = ann1.get_move(self.env) if self.env.player == 1 else ann2.get_move(self.env)
+            self.env.move(action)
+            if display: self.env.draw(0.2)
+        p1_won = True if self.env.result() == 1 else False
+        if p1_won:
+            return "1 won, " + str(j) + " moves."
+        else:
+            return "2 won, " + str(j) + " moves."
 
 class Agent:
     def __init__(self, ANN, training_level):
@@ -45,31 +48,23 @@ class Agent:
 
 if __name__ == '__main__':
     # TOPP parameters
-    games_to_be_played = 10
-    board_size = 4
+    board_size = 5
 
-    # MCTS/RL parameters
-    episodes = 20
-    simulations = 500
-    training_batch_size = 10
-    ann_save_interval = 5
-    eps = 1
-    eps_decay = 0.95
-
-    # ANN parameters
     activation_functions = ["linear", "sigmoid", "tanh", "relu"]
     optimizers = ["Adagrad", "SGD", "RMSprop", "Adam"]
-    alpha = 0.001 # learning rate
-    hidden_layer_sizes = [10,10,10]
-    io_layer_size = board_size * board_size # input and output layer sizes (always equal)
-    activation_func = activation_functions[3]
+    alpha = 0.01  # learning rate
+    H_dims = [math.floor(2*(1+board_size**2)/3)+board_size**2] * 3
+    io_dim = board_size * board_size  # input and output layer sizes
+    activation = activation_functions[3]
     optimizer = optimizers[0]
     epochs = 10
 
-    model = ANN(io_layer_size, hidden_layer_sizes, None, alpha, epochs, activation_func, optimizer)
-    RL_algorithm(episodes, simulations, training_batch_size, board_size, ann_save_interval, model, eps, eps_decay)
-    players = []
-    for level in level_models:
-        players.append(Agent(level_models[level], level))
+
+    models = [0,50,100,150,200]
+    players = {}
+    for level in models:
+        ann = ANN(io_dim, H_dims, alpha, optimizer, activation, epochs)
+        ann.load(board_size, level)
+        players[level] = ann
     tournament = TOPP(players, board_size)
-    tournament.run_tournament(games_to_be_played)
+    tournament.run_tournament()
